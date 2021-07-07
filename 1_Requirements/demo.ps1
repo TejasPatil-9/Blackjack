62a054de7d9a466ab1ddf72b3eeff8168b81434a
Tejas Patil <tejasrajeshp@gmail.com>
2021-07-07 11:57:50 +0530

    Updated!

--- 1_Requirements/demo.ps1
+++
@@ -1,2 +0,0 @@
-demo.ps1    
-This is a demo file
---
+++ 3_Implementation/inc/fun.h
@@ -0,0 +1,7 @@
+#ifndef __FUN_H
+#define __FUN_H
+    void show_record();
+    void reset_score();
+    void help();
+    void edit_score(float ,char[]);
+#endif
